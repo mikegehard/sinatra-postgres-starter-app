4b6563a8a45fabecaa4f719b1e6b951b5402fd0a
Mike Gehard <mike.gehard@gmail.com>
2014-04-14 14:49:14 -0600

    Fix issue with migrating locally

--- lib/tasks/db.rb
+++ lib/tasks/db.rb
@@ -10,7 +10,9 @@ namespace :db do
     environment = ENV['RACK_ENV'] || 'development'
     version = args[:version]
     migrations_directory = 'migrations'
-    connection_string = ENV.fetch('DATABASE_URL') || ENV.fetch("DATABASE_URL_#{environment.upcase}")
+    connection_string = ENV['DATABASE_URL'] || ENV["DATABASE_URL_#{environment.upcase}"]
+
+    raise "Missing Connection string" if connection_string.nil?
 
     db = Sequel.connect(connection_string)
     message = if args[:version].nil?
